5d6595484014d66ee30dc9a3cf0a7fbbf51c088e
3 (finch) <94497837+westwardfishdme@users.noreply.github.com>
2025-08-24 08:12:08 -0500

    fix #3, changed file shredding.

--- src/overwrite.rs
+++ src/overwrite.rs
@@ -1,46 +1,21 @@
-use rand::{self, Rng};
+use rand::{Fill, Rng};
 use std::fs::{File, Metadata};
 use std::io::{Seek, SeekFrom, Write};
 use std::sync::mpsc;
 use std::thread;
 
 fn fill_vector_random(mut vec: Vec<u8>, size: usize, opt: u8) -> Vec<u8> {
-    let mut q = rand::rng();
-    while vec.len() != size {
-        let x: u8;
-        match opt {
-            0 => {
-                x = q.random_range(0..255);
-                vec = vec![x; size]
-            }
-            1 => {
-                x = 0;
-                vec = vec![x; size];
-            }
-            _ => todo!(),
-        }
+    let x = 0;
+    vec = vec![x; size];
+
+    if opt == 0 {
+        rand::fill(&mut vec[..]);
     }
+
     return vec;
 }
-fn fill_vector_pattern(mut vec: Vec<u8>, size: usize, opt: u8) {}
 
 fn rand_overwrite(size: usize, mut file: &File, opt: u8) {
-    let (tx, rx) = mpsc::channel();
-    let (dtx, drx) = mpsc::channel();
-    let vec: Vec<u8> = Vec::new();
-    tx.send(vec).unwrap();
-    //start a thread to start the shredding.
-    let handle = thread::spawn(move || {
-        let mut data = rx.recv().unwrap();
-        data = fill_vector_random(data, size, opt);
-        dtx.send(data).unwrap();
-    });
-    handle.join().unwrap();
-    let content = drx.recv().unwrap();
-    file.write_all(content.as_slice()).unwrap();
-}
-
-fn pattern_overwrite(size: usize, mut file: &File, opt: u8) {
     let (tx, rx) = mpsc::channel();
     let (dtx, drx) = mpsc::channel();
     let vec: Vec<u8> = Vec::new();
@@ -66,8 +41,12 @@ pub fn large_overwrite(mut file: &File, file_metadata: &Metadata, zero: bool) {
 
     file.seek(SeekFrom::Start(0)).unwrap();
     if file_metadata.len() > 4096 {
-        let m = (file_metadata.len() / 4096) + 1;
-        start_iterations *= m;
+        let mut multiplier = file_metadata.len() / 4096;
+        if file_metadata.len() % 64 != 0 {
+            multiplier = (file_metadata.len() / 4096) + 1;
+        }
+        start_iterations *= multiplier;
+        dbg!(&start_iterations, multiplier);
     }
     rand_overwrite(start_iterations as usize, file, opt);
 }
